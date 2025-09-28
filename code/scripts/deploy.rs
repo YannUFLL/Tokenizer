1b396f2165152d8304c18aaa99170f4d084a4f2d
YannUFLL <yannlescoul79@free.fr>
2025-09-28 19:09:59 +0200

    Initial commit

---
+++ code/scripts/deploy.rs
@@ -0,0 +1,28 @@
+import { createWalletClient, http, publicActions } from "viem";
+import { hardhat } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+import { abi, bytecode } from "../artifacts/contracts/MyToken42.sol/MyToken42.json";
+
+async function main() {
+  const account = privateKeyToAccount(
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  );
+
+  const client = createWalletClient({
+    account,
+    chain: hardhat,
+    transport: http("http://127.0.0.1:8545"),
+  }).extend(publicActions);
+
+  const hash = await client.deployContract({
+    abi,
+    bytecode,
+    args: [1000], // approvisionnement initial
+  });
+
+  const receipt = await client.waitForTransactionReceipt({ hash });
+  console.log("Token déployé à:", receipt.contractAddress);
+}
+
+main().catch(console.error);
+
