41900adca6c2c9b1927df4391074b578ca5bc44b
YannUFLL <yannlescoul79@free.fr>
2026-01-25 00:37:33 +0100

    feat: implement YannCoin42 ERC-20 contract and hardhat config

--- code/hardhat.config.js
+++ code/hardhat.config.js
@@ -1,7 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-viem");
 
+const { vars } = require("hardhat/config");
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: vars.get("ETHERSCAN_API_KEY"), // Il te faudra une clé Etherscan gratuite
+  },
 };
